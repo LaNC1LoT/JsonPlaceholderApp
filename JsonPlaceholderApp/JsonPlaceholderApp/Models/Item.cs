6fe9b05d230635145c47fe3ef061c3c5ba755c5d
Александр Чолак <lanc1lot93@gmail.com>
2019-11-16 21:19:41 +0300

    Add project files.

---
+++ JsonPlaceholderApp/JsonPlaceholderApp/Models/Item.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JsonPlaceholderApp.Models
+{
+    public class Item
+    {
+        public Guid Id { get; set; }
+        public string Text { get; set; }
+        public string Description { get; set; }
+        public byte[] Image { get; set; }
+    }
+}
---
+++ JsonPlaceholderApp/JsonPlaceholderApp/Views/ItemsPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using JsonPlaceholderApp.Models;
+using JsonPlaceholderApp.Views;
+using JsonPlaceholderApp.ViewModels;
+
+namespace JsonPlaceholderApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ItemsPage : ContentPage
+    {
+        ItemsViewModel viewModel;
+
+        public ItemsPage()
+        {
+            InitializeComponent();
+
+            BindingContext = viewModel = new ItemsViewModel();
+        }
+
+        private void OnItemSelected(object sender, SelectedItemChangedEventArgs args)
+        {
+            if (!(args.SelectedItem is Item item))
+                return;
+
+            //await Navigation.PushAsync(new ItemDetailPage(new ItemDetailViewModel(item)));
+
+            ItemsListView.SelectedItem = null;
+        }
+
+        //async void AddItem_Clicked(object sender, EventArgs e)
+        //{
+        //    await Navigation.PushModalAsync(new NavigationPage(new NewItemPage()));
+        //}
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            if (viewModel.Items.Count == 0)
+                viewModel.LoadItemsCommand.Execute(null);
+        }
+    }
+}
